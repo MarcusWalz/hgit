13012a64d950dec317d3f111248814133049cef8
Marcus Walz <mewalz@uwm.edu>
2011-08-27 00:09:38 -0500

    basic tree reading

--- Data.hs
+++ Data.hs
@@ -7,6 +7,7 @@ import qualified Control.Exception as C
 import           Control.Monad.Reader
 import           Data.ByteString.Char8 (ByteString)
 import           Data.Maybe
+import           Data.List (find)
 import qualified Data.ByteString.Char8 as B
 
 data GitConfig  = GitConfig
@@ -66,6 +67,7 @@ data GitObject =
                | Blob   BlobID
                | Tree   TreeID
                | Tag    TagID
+  deriving (Show)
 
 data Person = Person 
   { personName  :: ByteString
@@ -81,3 +83,55 @@ data Commitent = Commitent
   , ceCommitterTime :: ByteString 
   , ceCommitMsg     :: ByteString
   } deriving (Show)
+
+data TreeNode = TreeNode
+  { mode      :: Int
+  , object    :: GitObject
+  , name      :: ByteString }
+  deriving (Show)
+
+readObjStr' :: ByteString -> Maybe GitObject
+readObjStr' str = 
+  readObjStr s objT 
+  where w = B.words str 
+        s = head w
+        objT = last w
+
+--wtf!!!!!!!!!
+
+objReader :: [ (ByteString, ID -> GitObject) ]
+objReader = [ (B.pack "commit" , Commit )
+            , (B.pack "blob"   , Blob   )
+            , (B.pack "tag"    , Tag    )
+            , (B.pack "tree"   , Tree   ) ]
+
+readObjStr :: ByteString -> ID -> Maybe GitObject
+readObjStr t id = find (\(x,n) -> t == x) objReader >>= \(x,n) -> Just (n id)
+
+readTreeNodeLine :: ByteString -> TreeNode
+readTreeNodeLine str =
+  TreeNode {mode = m, object = o, name = n}
+  where m = fst $ fromJust $ B.readInt $ head w 
+        o = fromJust $ readObjStr' $ B.unwords $ take 2 $ drop 1 w
+        n = B.unwords $ drop 3 w
+        w = B.words str
+
+data Trees = Trees [TreeNode]
+  deriving (Show)
+
+readProc :: (ByteString -> a) -> Handle -> IO [a]
+readProc f h = do
+  eof <- hIsEOF h
+  if eof 
+    then do hClose h
+            return [] 
+    else do a <- B.hGetLine h
+            m <- readProc f h
+            return $ [f a] ++ m 
+  
+readTree :: GitReader [TreeNode]
+readTree = do
+  (_,outh,_,_) <- spawnGitProcess cmd
+  x <- liftIO $ readProc readTreeNodeLine outh
+  return x
+  where cmd = makeGitCommand (B.pack "ls-tree") [B.pack "HEAD"]
