7e7845bb74b41a6276ce4202928e1f83bf0323bc
Marcus Walz <mewalz@uwm.edu>
2011-08-26 20:41:10 -0500

    git datatypes added

--- Data.hs
+++ Data.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving #-}
+
 import           System.Command
 import           System.IO
+import           Control.Concurrent
+import qualified Control.Exception as C
 import           Control.Monad.Reader
 import           Data.ByteString.Char8 (ByteString)
 import           Data.Maybe
@@ -8,24 +12,72 @@ import qualified Data.ByteString.Char8 as B
 data GitConfig  = GitConfig
   { gitCwd   :: FilePath 
   , gitPath  :: Maybe FilePath }
+  deriving (Show)
 
 makeGitConfig :: FilePath -> Maybe FilePath -> GitConfig
 makeGitConfig cwd' gitPath' = GitConfig {gitCwd = cwd', gitPath = gitPath'}
 
 data GitCommand = GitCommand
   { gitCmd  :: ByteString 
-  , args :: [ByteString]
-  }
-
+  , args :: [ByteString] }
+  deriving (Show)
+  
 makeGitCommand :: ByteString -> [ByteString] -> GitCommand 
 makeGitCommand cmd args' = GitCommand {gitCmd = cmd, args = args'}
 
-createGitProcess :: GitConfig -> GitCommand -> CreateProcess
-createGitProcess GitConfig  { gitCwd = gitCwd', gitPath = gitPath }
+newtype GitReader a = GitReader (ReaderT GitConfig IO a)
+  deriving (Monad, MonadIO, MonadReader GitConfig)
+
+createGitProcess' :: GitConfig -> GitCommand -> CreateProcess
+createGitProcess' GitConfig  { gitCwd = gitCwd', gitPath = gitPath }
                  GitCommand { gitCmd = cmd, args = args'}
- = (proc (fromMaybe "git" gitPath) args'')
- { std_in = CreatePipe 
- , std_out = CreatePipe 
- , std_err = CreatePipe 
- , close_fds = True }
- where args'' = map B.unpack $ [cmd] ++ args'
+  = (proc (fromMaybe "git" gitPath) args'')
+  { std_in = CreatePipe 
+  , cwd = Just gitCwd' 
+  , std_out = CreatePipe 
+  , std_err = CreatePipe 
+  , close_fds = True }
+  where args'' = map B.unpack $ [cmd] ++ args'
+
+createGitProcess :: GitCommand -> GitReader CreateProcess 
+createGitProcess command = do
+  conf <- ask
+  return $ createGitProcess' conf command
+
+-- This function works because of Magic!!! 
+spawnGitProcess :: GitCommand 
+                -> GitReader (Handle, Handle, Handle, ProcessHandle)
+spawnGitProcess command = do
+  proc <- createGitProcess command 
+  (Just inh, Just outh, Just errh, pid) <- liftIO $ createProcess $ proc 
+  return (inh, outh, errh, pid)
+
+runGit :: GitConfig -> GitReader t -> IO t
+runGit config (GitReader a) = runReaderT a config
+
+type ID       = ByteString
+type CommitID = ID 
+type BlobID   = ID 
+type TreeID   = ID 
+type TagID    = ID
+
+data GitObject = 
+                 Commit CommitID 
+               | Blob   BlobID
+               | Tree   TreeID
+               | Tag    TagID
+
+data Person = Person 
+  { personName  :: ByteString
+  , personEmail :: ByteString
+  } deriving (Show)
+
+data Commitent = Commitent
+  { ceParents       :: [CommitID]
+  , ceTree          :: TreeID
+  , ceAuthor        :: Person
+  , ceAuthorTime    :: ByteString
+  , ceCommitter     :: Person
+  , ceCommitterTime :: ByteString 
+  , ceCommitMsg     :: ByteString
+  } deriving (Show)
