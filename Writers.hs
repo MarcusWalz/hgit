f46482d27d12049fcf2c7e7aab45d07eb2420392
Marcus Walz <mewalz@uwm.edu>
2011-08-29 02:39:25 -0500

    testing commit

--- Writers.hs
+++ Writers.hs
@@ -1,3 +1,16 @@
+module Lib.HGit.Writers
+  ( runGitCommand
+  , gitInit
+  , gitClone
+  , gitAdd
+  , gitAddFiles
+  , gitMv
+  , gitRm
+  , gitCommit
+  , gitTagNew
+  , gitTagRm
+  ) where
+
 import           System.Command
 import           System.IO
 import           Control.Monad.Reader
@@ -24,6 +37,9 @@ runGitCommand cmd = do
         T.putStrLn e
         return False
 
+gitInit :: GitReader Bool
+gitInit = runGitCommand $ makeGitCommand (T.pack "init") []
+
 gitClone :: Text -> GitReader Bool
 gitClone url = runGitCommand cmd
   where cmd = makeGitCommand (T.pack "clone") [url]
@@ -35,9 +51,27 @@ gitAddFiles :: [FilePath] -> GitReader Bool
 gitAddFiles files = runGitCommand cmd
   where cmd = makeGitCommand (T.pack "add") $ map T.pack files
 
+gitMv :: FilePath -> FilePath -> GitReader Bool
+gitMv source destination = runGitCommand cmd
+  where cmd = makeGitCommand (T.pack "mv") $ map T.pack ["-f", source, destination]
+
+gitRm :: FilePath -> GitReader Bool
+gitRm file = runGitCommand $ makeGitCommand (T.pack "rm") [T.pack file]
+
 gitCommit :: Maybe FilePath -> Text -> GitReader Bool
 gitCommit file message = runGitCommand cmd
   where cmd = makeGitCommand (T.pack "commit") args
         args = f ++ [T.pack "-m", message]
         f = maybe [T.pack "-a"] f' file
         f' z = [T.pack "-f", T.pack z]
+
+gitTagNew :: Text -> Maybe Text -> Maybe CommitID -> GitReader Bool
+gitTagNew title message commit = runGitCommand cmd  
+  where cmd = makeGitCommand (T.pack "tag") args
+        args = [title] ++ msg ++ comm
+        comm = maybe [] singleton commit 
+        msg = maybe [] (\x -> T.pack "-m" : singleton x) message
+        singleton x = [x]
+        
+gitTagRm :: Text -> GitReader Bool
+gitTagRm tag = runGitCommand $ makeGitCommand (T.pack "tag") [T.pack "-d", tag]
